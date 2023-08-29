25fc75e02905ebcad415a9799a8fb513489815fa
Abdullah Farahat <abdullahfarahat18@gmail.com>
2023-08-29 11:35:54 -0700

    this commit my look obsulete but it is my savior

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - function prints the alphabet in lower case
+ * main - Prints the alphabet in lowercase.
  *
- * Returns: Always 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	char alpha;
