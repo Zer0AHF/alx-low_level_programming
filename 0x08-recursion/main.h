161c43f02690eaa1e016f3e16c80d6ae591e03b9
Abdullah Farahat <abdullahfarahat18@gmail.com>
2023-08-31 16:37:32 -0700

    recursion in c tasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,8 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
-int wildcmp(char *s1, char *s2);
 int is_prime_number(int n);
-int is_palindrome(char *s);
 
 #endif
