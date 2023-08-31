f04a9d1992140a9f45e31f9ea9d415bfb92e331e
Abdullah Farahat <abdullahfarahat18@gmail.com>
2023-08-31 16:49:08 -0700

    recursion in c tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
+/**
  * _puts_recursion - print a string.
- * @s: points to a char
- * Return: None.
+ * @s: pointer to char
+ * Return: No.
  */
 
 void _puts_recursion(char *s)
